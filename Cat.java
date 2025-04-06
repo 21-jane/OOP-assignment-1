562e8a58943ec49c7df2c25d48ec65275e0b1f1a
Jane <jane.kiboi@strathmore.edu>
2025-04-06 22:33:17 +0300

    Create Cat.java

---
+++ Cat.java
@@ -0,0 +1,9 @@
+// Interface for a Cat
+public interface Cat {
+    // Method that any cat must use to make a sound
+    void makeSound();
+
+    // Method that shows what the cat does
+    void performAction();
+}
+
